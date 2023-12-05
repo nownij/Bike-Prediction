ec8f38342c107d3c641b7ff9328f5dfc88dea14e
LEEJINWON <myjames3564@gmail.com>
2023-12-05 22:55:43 +0900

    commit :)

--- Functions/Reader.py
+++ Functions/Reader.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
+# pd.set_option('display.max_rows', None)
+# pd.set_option('display.max_columns', None)
 
 newColumns = ['Date', 'HHMM', 'ST-start', 'ST-end', 'Use', 'Minute[min]', 'Distance[m]']
 
@@ -7,11 +11,16 @@ def showCSV(file_path):
     data.columns = newColumns
 
     return data
+# ======================================================================== #
+
+
 def dataOptimization(DataFrame):
     DataFrame = selectLocation(DataFrame)
     DataFrame = sameST(DataFrame) # 대여한 곳에서 바로 자전거를 반납한 케이스 제거
     DataFrame = brokenBike(DataFrame) # 자전거가 고장나서 센터로 반납되는 케이스 제거
     DataFrame.reset_index(drop=True, inplace=True) # row 개수에 맞는 index 초기화
+
+    print(DataFrame)
 
     return DataFrame
 def selectLocation(DataFrame):
@@ -28,15 +37,6 @@ def selectLocation(DataFrame):
     DataFrame = DataFrame[fullCase]
 
     return DataFrame
-
-    # 구, 동 선택하는 방법? 커맨드에서 받거나 시각화해서 클릭으로 설정하던가 일단 광진구 건대 쪽만 해보자
-
-    # 광진구 내부에서만 움직이는 거
-    # 광진구에서 빠져나가는거
-    # 광진구로 들어오는거
-
-
-    pass
 def sameST(DataFrame):
     min_t = 5
     min_dst = 10
@@ -55,5 +55,47 @@ def brokenBike(DataFrame):
 
     # Apply conditions and return the modified DataFrame
     return DataFrame[~drops]
+# ======================================================================== #
 
 
+def showGraph(DataFrame):
+    interval = int(input("Input Interval : "))
+    usageOverTime(DataFrame, interval)
+def usageOverTime(DataFrame, interval):
+    DataFrame = DataFrame.drop(['ST-start', 'ST-end', 'Minute[min]', 'Distance[m]'], axis=1)
+    resultDF = DataFrame.groupby(['Date', 'HHMM'], as_index=False)['Use'].sum()
+
+    # 'HHMM' changes to str
+    resultDF['HHMM'] = resultDF['HHMM'].astype(str).str.zfill(4)
+    resultDF['Datetime'] = pd.to_datetime(resultDF['Date'].astype(str) + resultDF['HHMM'], format='%Y%m%d%H%M')
+
+    # set index to 'Datetime'
+    resultDF.set_index('Datetime', inplace=True)
+
+    # set interval
+    itVL = str(interval) + 'T'
+    resampledDF = resultDF.resample(itVL).sum()
+
+    # reset index
+    resampledDF.reset_index(inplace=True)
+    resampledDF['HHMM'] = resampledDF['Datetime'].dt.strftime('%H%M').astype(int)
+
+    resampledDF = resampledDF[['Date', 'HHMM', 'Use']]
+
+    print(resampledDF)
+
+    # graph Settings
+    plt.figure(figsize=(12, 6))
+    plt.plot(resampledDF['HHMM'], resampledDF['Use'], linestyle='solid')
+
+    # graph title, label
+    plt.title('Hourly Use Over Time', fontsize=16)
+    plt.xlabel('HHMM', fontsize=14)
+    plt.ylabel('Use', fontsize=14)
+
+    # show Graph
+    plt.grid(True)
+    plt.show()
+
+
+# ======================================================================== #
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from Functions import Reader as Rd
-import pandas as pd
 import parameters as pr
 
 if __name__ == "__main__":
     data = Rd.showCSV(pr.file_path)
     op = Rd.dataOptimization(data)
 
-    print(op)
+    Rd.showGraph(op)
