b1374840fa3fea4f6203e556f696b3fd3a0bd91c
LEEJINWON <myjames3564@gmail.com>
2023-12-17 12:56:57 +0900

    commit :)

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from Functions import DateUtils as DU
-from Functions import DateMerger as DM
-from Functions import GraphPlotter as GP
-
-import pandas as pd
-
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
+from Functions import dataProcessor, visualizer
 
 if __name__ == "__main__":
-    start_date, end_date, filename_result = DU.DateRangeSettings()
-    merged_df = DM.MergeDataframes(filename_result)
-    GP.ShowGraph(start_date, end_date, merged_df)
-    print(merged_df) # 2월 데이터도 이상함 전수 조사 필요
+    date_range = dataProcessor.select_date()
+
+    original_data = dataProcessor.read_csv_files_in_date_range(date_range)
+    #print(original_data)
+
+    set_col_data = dataProcessor.set_columns(original_data)
+    #print(set_col_data)
+
+    removed_data = dataProcessor.remove_useless_data(set_col_data)
+    #print(removed_data)
+
+    target_data = dataProcessor.set_target_location(removed_data)
+    #print(target_data)
+
+    final = dataProcessor.merge_dataFrames(target_data)
+    #print(final)
+
+    visualizer.show_dataframe(date_range ,final)
---
+++ params.py
@@ -0,0 +1,4 @@
+year = "2023"
+base_name = f"tpss_bcycl_od_statnhm_{year}"
+base_route = "C:/Users/myjam/Desktop"
+
